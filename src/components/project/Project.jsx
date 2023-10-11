d3f1f6973cd13e93396a92da89245ed35282fac9
shashanka <shashankaadiga01@gmail.com>
2023-10-11 11:26:57 +0530

    modified alt tags in image

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -10,13 +10,13 @@ function Project() {
     <div className="flex flex-col items-center justify-center w-full px-4 py-6">
       <div className={`${styles.project_grid} `}>
         <div className=" group shadow-xl hover:bg-green-500 rounded p-1 bg-slate-700">
-         <Link href={'https://react-fitness-app-five.vercel.app/'}><Image src={img1} className="h-full " /></Link> 
+         <Link href={'https://react-fitness-app-five.vercel.app/'}><Image src={img1} alt="project" className="h-full " /></Link> 
         </div>
         <div className="shadow-xl hover:bg-green-500 rounded p-1 bg-slate-700">
-        <Link href={'https://react-movie-app-phi-orcin.vercel.app/'}><Image src={img2} className="h-full " /></Link>  
+        <Link href={'https://react-movie-app-phi-orcin.vercel.app/'}><Image src={img2} alt="project" className="h-full " /></Link>  
         </div>
         <div className="shadow-xl hover:bg-green-500 rounded p-1 bg-slate-700">
-         <Link href={'https://react-wordle-murex-alpha.vercel.app/'}><Image src={img3} className="h-full" /></Link> 
+         <Link href={'https://react-wordle-murex-alpha.vercel.app/'}><Image src={img3} alt="project" className="h-full" /></Link> 
         </div>
        
       </div>
